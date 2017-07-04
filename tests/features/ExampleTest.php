b1d4a280d4837e24bee8a4de41c602d8179d218b
Rafa Salinas <rsalinas31@gmail.com>
2017-07-04 17:54:36 +0200

    Fix composer.json autoload FeatureTestCase

--- tests/features/ExampleTest.php
+++ tests/features/ExampleTest.php
@@ -1,18 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class ExampleTest extends TestCase
+class ExampleTest extends FeatureTestCase
 {
-    use DatabaseTransactions;
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
+    function test_basic_example()
     {        
 
         $name = 'Rafael Salinas';
